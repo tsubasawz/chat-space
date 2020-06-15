e1148ca8f89f95d69d8a170c27839e9e4cede235
tsubasawz <65057561+tsubasawz@users.noreply.github.com>
2020-06-15 12:35:42 +0900

    Asynchronous

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -40,27 +40,26 @@ $(function(){
    };
  }
 $('#new_message').on('submit', function(e){
- e.preventDefault();
- var formData = new FormData(this);
- var url = $(this).attr('action')
- $.ajax({
-   url: url,
-   type: "POST",
-   data: formData,
-   dataType: 'json',
-   processData: false,
-   contentType: false
- })
-  .done(function(data){
-    var html = buildHTML(data);
-    $('.messages').append(html);
-    $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-    // $('form')[0].reset();
-    $('.form__submit').prop('disabled', false);
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action')
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
   })
-  .fail(function() {
-    alert("メッセージ送信に失敗しました");
-  });
-  return false;
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      $('.form__submit').prop('disabled', false);
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    });
+    return false;
   }); 
 });
